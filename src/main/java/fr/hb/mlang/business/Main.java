19058cc7d60e70d98e6997170bb06a5e811dfcf8
Mathieu Langumier <mathieu.langumier@gmail.com>
2025-06-25 16:15:34 +0200

    Feat: set up Singleton for EntityManagerFactory

--- src/main/java/fr/hb/mlang/business/Main.java
+++ src/main/java/fr/hb/mlang/business/Main.java
@@ -1,19 +1,16 @@
 package fr.hb.mlang.business;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.Persistence;
+import fr.hb.mlang.business.utils.JpaFactory;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-
-        EntityManager em = Persistence.createEntityManagerFactory("PROJECT_ORM_PU").createEntityManager();
 
         //TODO:
         // - Set up EntityManagerFactory (singleton)
         // - Test a full CRUD on USER
         // - Refactorize CRUD to a GenericCrudInterface/Impl
 
-        em.close();
+        JpaFactory.close();
     }
 }
---
+++ src/main/java/fr/hb/mlang/business/utils/JpaFactory.java
@@ -0,0 +1,36 @@
+package fr.hb.mlang.business.utils;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+/**
+ * JPA EntityManagerFactory implementation.
+ *
+ * @implNote Singleton
+ */
+public class JpaFactory {
+    private static final String PERSISTENCE_UNIT_NAME = "PROJECT_ORM_PU";
+    private static final EntityManagerFactory factory;
+
+    private JpaFactory() {
+    }
+
+    static {
+        try {
+            factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+        } catch (Exception e) {
+            throw new ExceptionInInitializerError("\n>>> Initial EntityManagerFactory creation failed: " + e);
+        }
+    }
+
+    public static EntityManager getEntityManager() {
+        return factory.createEntityManager();
+    }
+
+    public static void close() {
+        if (factory != null && factory.isOpen()) {
+            factory.close();
+        }
+    }
+}
